c37f2a21d09d3612263b40de1d8c02e43938c36b
bates232 <bates232@unicaen.fr>
2025-12-10 09:55:31 +0100

    file creation and drizzle import

---
+++ README.md
@@ -0,0 +1,4 @@
+https://clementcatel.notion.site/R5-05-Projet-de-groupe-2ae3b8266dbb8014b0aac3869c316f7c
+
+.env
+DB_FILE=file:local.db
---
+++ src/db/schema.js
@@ -0,0 +1,4 @@
+import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
+import { randomUUID } from 'crypto'
+import { minLength, number } from "zod";
+
