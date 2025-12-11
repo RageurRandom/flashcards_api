8639623ab0197432fc0f7ffad81ad0cabf61acf4
Leo Berranger <berrang232@unicaen.fr>
2025-12-11 09:16:15 +0100

    feat: added router and controller for authentication

---
+++ src/controller/authController.js
@@ -0,0 +1,25 @@
+import { request, response } from "express"
+
+
+/**
+ * 
+ * @param {request} req 
+ * @param {response} res 
+ * @returns 
+ */
+export const registerUser = async (req, res)=>{
+    res.status(200).send({message : "WIP"})
+    //TODO
+}
+
+
+/**
+ * 
+ * @param {request} req 
+ * @param {response} res 
+ * @returns 
+ */
+export const loginUser = async (req, res)=>{
+    res.status(200).send({message : "WIP"})
+    //TODO
+}
--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import cardRouter from './routes/cardsRouter.js'
+import collectionRouter from './routes/collectionsRouter.js'
+import authRouter from './routes/authRouter.js'
 
 const PORT = process.env.PORT || 3000
 
@@ -8,6 +10,8 @@ const app = express()
 app.use(express.json())
 
 app.use('/cards', cardRouter)
+app.use('/collections', collectionRouter)
+app.use('/auth', authRouter)
 
 app.listen(PORT, ()=> {
     console.log(`Server running on http://localhost:${PORT}`)
